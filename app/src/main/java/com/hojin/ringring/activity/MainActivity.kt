256c8a7bb5cd0e98041f7b9220c795bbf970b931
cri3958 <leehojin0207@naver.com>
2021-05-14 21:45:25 +0900

    서비스 잠시 멈추기 틀은 완성, 기능은 내일~

--- app/src/main/java/com/hojin/ringring/activity/MainActivity.kt
+++ app/src/main/java/com/hojin/ringring/activity/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.hojin.ringring.activity
 
+import android.app.AlertDialog
 import android.app.NotificationManager
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.hojin.ringring.PhoneBook.PhoneBookActivity
 import com.hojin.ringring.R
 import com.hojin.ringring.service.RingService
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.dialog_timer.*
+import kotlinx.android.synthetic.main.dialog_timer.view.*
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
@@ -33,6 +40,7 @@ class MainActivity : AppCompatActivity() {
         if(!file.exists()){//파일이 없으면
             val outFs: FileOutputStream = openFileOutput(fileName, Context.MODE_PRIVATE)
             val str="OFF@"
+            btn_timer.visibility = View.GONE
             outFs.write(str.toByteArray())
             outFs.close()
         }else{//파일이 있으면
@@ -44,6 +52,7 @@ class MainActivity : AppCompatActivity() {
             if(temp[0].equals("ON")){
                 startForegroundService( Intent(this, RingService::class.java))
                 switch_service.isChecked = true
+                btn_timer.visibility = View.VISIBLE
             }
         }
 
@@ -59,13 +68,62 @@ class MainActivity : AppCompatActivity() {
                 startForegroundService( Intent(this, RingService::class.java))
                 Toast.makeText(applicationContext,"서비스 시작",Toast.LENGTH_SHORT).show()
                 str = "ON@"
+                btn_timer.visibility = View.VISIBLE
             }else{//OFF
                 stopService(Intent(this,RingService::class.java))
                 Toast.makeText(applicationContext,"서비스 종료",Toast.LENGTH_SHORT).show()
                 str = "OFF@"
+                btn_timer.visibility = View.GONE
             }
             outFs.write(str.toByteArray())
             outFs.close()
         }
+
+        btn_timer.setOnClickListener { dialog(this) }
     }
+
+    fun dialog(context: Context){
+        val list_of_text = arrayOf("10분","30분","1시간","2시간","3시간","직접입력")
+        val list_of_time = arrayOf(10,30,60,120,180)
+        var time:Int=0
+
+        val view:View = layoutInflater.inflate(R.layout.dialog_timer,null)
+
+
+        view.timer_spinner.adapter = ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,list_of_text)
+        view.timer_spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                time=0
+            }
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
+                if(position!=5) { //값이 "기타"이지 않으면
+                    time = list_of_time[position]
+                    view.timer_input.text = null
+                    view.timer_input.visibility = View.GONE
+                }else{
+                    view.timer_input.visibility = View.VISIBLE
+                }
+            }
+        }
+
+        val builder:AlertDialog.Builder = AlertDialog.Builder(context)
+
+        builder.setTitle("서비스 잠시 멈추기")
+        builder.setView(view)
+        builder.setPositiveButton("확인"){dialog,which ->
+            if(view.timer_input.text.isEmpty()) {//기타가 아닐경우
+                Toast.makeText(context, time.toString() + "분동안 서비스 대기", Toast.LENGTH_SHORT).show()
+            }else{
+                time = Integer.parseInt(view.timer_input.text.toString())
+                Toast.makeText(context, time.toString() + "분동안 서비스 대기", Toast.LENGTH_SHORT).show()
+            }
+        }
+        builder.setNegativeButton("취소",null)
+        builder.setCancelable(false)
+        builder.create()
+
+        builder.show()
+
+    }
+
 }
